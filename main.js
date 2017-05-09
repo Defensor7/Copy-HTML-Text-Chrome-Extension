716d8850e5d9933e2d6cd6cb4c3107725d414110
CORP\jcheuk <jcheuk@Infusion.com>
2017-05-09 11:05:35 -0400

    Added chrome extension manifest & main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+promptSelectedHTML = function(){
+    
+    var HTMLText = getSelectionHtml();
+
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", HTMLText);
+}
+
+function getSelectionHtml() {
+    var html = "";
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement("div");
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            html = container.innerHTML;
+        }
+    } else if (typeof document.selection != "undefined") {
+        if (document.selection.type == "Text") {
+            html = document.selection.createRange().htmlText;
+        }
+    }
+    return html;
+}
+
+
+chrome.contextMenus.create({
+    title: "Get Selected HTML",
+    contexts: [],
+    onclick: promptSelectedHTML
+});
