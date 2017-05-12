c7848d89c01d7c3e79350925416a9b3ee6a2ae1e
joecheuk <cheukmankit12131@hotmail.com>
2017-05-11 21:33:29 -0400

    added return value from the injected script

--- main.js
+++ main.js
@@ -1,36 +1,14 @@
 promptSelectedHTML = function(){
-	
 	chrome.tabs.query({ currentWindow: true, active: true }, getTabs);
 }
 
 function getTabs(tabs) {
-	
-	chrome.tabs.executeScript(null, { file: "injected.js" });
-	
-	var HTMLText = getSelectionHTML(win);
-  
-	window.prompt("Copy to clipboard: Ctrl+C, Enter", HTMLText);
-}
-
-function getSelectionHTML(curWindows) {
-    var html = "";
-    if (typeof curWindows.getSelection != "undefined") {
-        var sel = curWindows.getSelection();
-        if (sel.rangeCount) {
-            var container = document.createElement("div");
-            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                container.appendChild(sel.getRangeAt(i).cloneContents());
-            }
-            html = container.innerHTML;
+	chrome.tabs.executeScript(null, { file: "injected.js" }, function(result){
+        if (result){  
+	        window.prompt("Copy to clipboard: Ctrl+C, Enter", result);
         }
-    } else if (typeof document.selection != "undefined") {
-        if (document.selection.type == "Text") {
-            html = document.selection.createRange().htmlText;
-        }
-    }
-    return html;
+    });
 }
-
 
 chrome.contextMenus.create({
     title: "Get Selected HTML",
